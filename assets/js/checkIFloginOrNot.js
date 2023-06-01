b3ccda79c81c291240f23b5f2b583c5d62163f33
jhon mark valdez <markbrvaldez@gmail.com>
2023-06-01 12:47:03 +0800

    update

--- assets/js/checkIFloginOrNot.js
+++ assets/js/checkIFloginOrNot.js
@@ -1,11 +1,16 @@
 firebase.auth().onAuthStateChanged(function (user) {
   const logoutDiv = document.getElementById('logout-button');
   const loginDiv = document.getElementById('login-button');
+  const verificationStatus = document.getElementById('verification-status');
   if (user) {
     loginDiv.style.display = 'none';
     logoutDiv.style.display = 'flex';
+    verificationStatus.display = 'flex';
   } else {
     loginDiv.style.display = 'flex';
     logoutDiv.style.display = 'none';
+  }
+  if (loginDiv.style.display == 'flex') {
+    verificationStatus.display = 'none';
   }
 });
