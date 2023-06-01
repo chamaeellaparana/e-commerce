30678058b3f9dd2bc683725544d5e33d347958ab
jhon mark valdez <markbrvaldez@gmail.com>
2023-06-01 12:18:32 +0800

    update

--- assets/js/checkIFloginOrNot.js
+++ assets/js/checkIFloginOrNot.js
@@ -1,8 +1,11 @@
 firebase.auth().onAuthStateChanged(function (user) {
   const loginDiv = document.getElementById('logout-button');
+  const logoutDiv = document.getElementById('logout-in');
   if (!user) {
-    loginDiv.textContent = 'LOGIN';
+    loginDiv.style.display = 'none';
+    logoutDiv.style.display = 'flex';
   } else {
-    loginDiv.textContent = 'lOGOUT';
+    loginDiv.style.display = 'flex';
+    logoutDiv.style.display = 'none';
   }
 });
